02ac0b7db5561114d90fac143be4eaad54575c35
Gunel Hesenzade <gunel.h@code.edu.az>
2022-04-08 21:19:11 +0300

    test

---
+++ VanillaJsFilteringData/scriptFunction.js
@@ -0,0 +1,12 @@
+
+
+// const list = require('./script')
+// function nameCountries (_obj){
+//    _objData = ""
+//    for(let _key in _obj){
+//       _objData += `$(_key)`;
+//    }
+//    console.log(_objData);
+// }
+
+// nameCountries(list.countries);

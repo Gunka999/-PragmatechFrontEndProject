49126054f3298c1edde43ce1e30727ed2c64d525
Gunel Hesenzade <gunel.h@code.edu.az>
2022-04-30 01:10:22 +0300

    update

--- TagSelectorBasic/tapsiriq 08/js/app.js
+++ TagSelectorBasic/tapsiriq 08/js/app.js
@@ -21,12 +21,19 @@ for (let aList of favoriteListText){
    favoriteList.innerHTML += list;
 }
 
-const ClickBtns = document.querySelectorAll(".favoriteList-btn")
+const ClickBtns = document.querySelectorAll(".favoriteList-btn");
 
 for (let i = 0; i < ClickBtns.length; i++) {
    ClickBtns[i].addEventListener("click", (e) => {
       if (!ClickBtns[i].classList.contains("addCSS")) {
          ClickBtns[i].classList.add("addCSS")
+         for(let j = 0; j < ClickBtns.length; j++){
+            if(j!=i){
+               if(ClickBtns[j].classList.contains("addCSS")){
+                  ClickBtns[j].classList.remove("addCSS")
+               }
+            }
+         }         
       } else {
          ClickBtns[i].classList.remove("addCSS")
       }
@@ -39,10 +46,27 @@ for (let i = 0; i < ClickI.length; i++) {
    ClickBtns[i].addEventListener("click", (e) => {
       if (!ClickI[i].classList.contains("ClickIAddCSS")) {
          ClickI[i].classList.add("ClickIAddCSS")
+         for(let j = 0; j < ClickI.length; j++){
+            if(j!=i){
+               if(ClickI[j].classList.contains("ClickIAddCSS")){
+                  ClickI[j].classList.remove("ClickIAddCSS")
+               }
+            }
+         } 
       } else {
          ClickI[i].classList.remove("ClickIAddCSS")
       }
    })
 }
+
+
+function myFunction(){
+   let x = document.getElementById("myText").value;
+   // document.getElementById("demo").innerHTML = x;
+ 
+   favoriteListText.push(x)
+}
+
+console.log(x)
 
 
